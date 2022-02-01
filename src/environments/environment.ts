f273c5d891bb32af414e220d95747859918287ca
DESKTOP-NLNSFMK\Kairit <kairit.semiskar@hotmail.com>
2022-02-01 10:43:29 +0200

    project upload

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,6 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
+  firebase: {
+    projectId: 'todo-app-8103d',
+    appId: '1:896321586913:web:9ffb1f9b030562e296d9a0',
+    storageBucket: 'todo-app-8103d.appspot.com',
+    apiKey: 'AIzaSyCeD_EX1HS-zsLZsedWgTX_Gskjp9zlJl0',
+    authDomain: 'todo-app-8103d.firebaseapp.com',
+    messagingSenderId: '896321586913',
+    measurementId: 'G-MP0CNR6WB0',
+  },
   production: false
 };
 
